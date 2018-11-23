1bce5efab321188b915ab68d26b52fe050ae0234
gfluis <45286637+gfluis@users.noreply.github.com>
2018-11-23 11:30:29 +0000

    Create Printf.js

---
+++ Printf.js
@@ -0,0 +1,21 @@
+// Simulators that are required for the simulation
+var simulators = [
+    {
+        name: "OPAL01",
+        state: "unconnected",
+        reconnect: 1,
+        msg_counter: 0,
+        simtime: -1,
+        actions: [],
+        messages: []
+    },
+    {
+        name: "MODELER01",
+        state: "unconnected",
+        reconect: 0,
+        msg_counter: 0,
+        simtime: -1,
+        actions: [],
+        messages: []
+    }
+];
